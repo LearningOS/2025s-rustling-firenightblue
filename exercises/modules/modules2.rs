a3c80b332b5efede2c3100595984b231e66201bb
root <root@localhost.localdomain>
2025-04-09 15:53:06 +0800

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,12 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-
+// I AM DONE
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as fruits;
-    use self::veggies::CUCUMBER as veggies;
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
